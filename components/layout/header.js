65cb8edf46020f0d5fd6f4cd665c1f04b3950be0
Fanger Zero <FangerZero@hotmail.com>
2023-01-24 10:19:27 -0600

    Updated Title

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 
 export default function Header(props) {
+  let genericTitle = 'Legend of Dragoon Community';
+  if (props.title && props.title !== '') {
+      genericTitle = `${props.title} - ${genericTitle}`;
+  }
+
   return (
     <Head>
-      <title>{props.title}</title>
+      <title>{genericTitle}</title>
       <meta name="description" content={props.description} />
       <link rel="icon" href="/favicon.ico" />
     </Head>
