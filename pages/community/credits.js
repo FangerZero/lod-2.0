6a549a07f6b2f4cead744cc0a8143a6d37d24729
Fanger Zero <FangerZero@hotmail.com>
2023-01-29 23:26:09 -0600

    Added Credit page, needs styling

---
+++ pages/community/credits.js
@@ -0,0 +1,121 @@
+import Header from '../../components/layout/header'
+
+export default function Credits() {
+    return (
+      <>
+      <Header title="Credits" description="Giving the community that helps make this game thrive years after others have forgotten it." />
+      <div class="entry-content wp-block-post-content is-layout-constrained">
+<p>This is a shortlist of known contributions to the Legend of Dragoon fandom, as it pertains to the global movement over these last few years. An automatic thanks go out to all legacy contributors across the web, from fansite managers to artists and musicians and writers and so much else in 20 years’ time. </p>
+
+
+
+<h2 class="wp-block-heading" id="website">Website</h2>
+
+
+
+<p>StephRose / DiehardDragoon: Sitemap assistance</p>
+
+
+
+<p>LostJourneyman &amp; PinballThief: Help planning a short, mid, and long-term plan for website frontend and backend design. </p>
+
+
+
+<p>Zychronix &amp; theflyingzamboni: Guest articles/how-to guides related to modding.</p>
+
+
+
+<h2 class="wp-block-heading" id="modding">Modding</h2>
+
+
+
+<p>Zychronix: for countless hours on Hard Mode and its successor Dragoon Modifier, help requests, and agreeing to help pave the way for unified inter-mod compatibility. Specialized in RAM editing. </p>
+
+
+
+<p>theflyingzamboni: For their script overhaul of Disc 1, ROM-editing tools, improving legacy mod compatibility, and the first texture mod. </p>
+
+
+
+<p>Massive credit to tfz and Zychronix for essentially taking on the entire Upscale Project themselves, paving the road for future graphics mods.</p>
+
+
+
+<p>Illeprih: For code implementation, cleaning up messy code, optimizations, et cetera. </p>
+
+
+
+<p>Monoxide: For committing Drew-level insanity by attempting to reverse-engineer the game. Much progress has been made over the last year. Potential benefits: eliminate load time, rendering control, advanced modding.</p>
+
+
+
+<p>DooMMetaL: For extensive research on 3D model analysis, and potential modding applications to that end.</p>
+
+
+
+<h2 class="wp-block-heading" id="gaming-news">Gaming News</h2>
+
+
+
+<p>StephRose / DiehardDragoon: for multiple quality reports on the Legend of Dragoon as a fandom, not just as “a bunch of fans who want it to make a comeback.” </p>
+
+
+
+<h2 class="wp-block-heading" id="community-moderation">Community Moderation</h2>
+
+
+
+<p>Community Contributors: all folks with this role in the Discord server have made significant impacts/contributions on the global fandom for the better. Both in their own works as well as helping discuss community improvements/challenges/issues. It’s too much to list here, so a warm thanks to each one of you over the years.</p>
+
+
+
+<p>SilentSkye: For handling spambots and occasional kicks/bans when necessary.</p>
+
+
+
+<p>Brady’sBrother: For general community nurturing.</p>
+
+
+
+<p>DrewUniverse: Community organizer, moderation, et cetera.</p>
+
+
+
+<h2 class="wp-block-heading" id="international">International</h2>
+
+
+
+<p>Ziekara and Char: For many, many hours spent translating and documenting various pieces of LoD media from Japanese to English, and sometimes vice-versa.</p>
+
+
+
+<h2 class="wp-block-heading">Fanart Usage Rights</h2>
+
+
+
+<p>Mr. Scentless: Dart, Rose, Lavitz, Meru (Discord Stickers and forum avatars)</p>
+
+
+
+<p>Gonzo: Dart Madness Hero (Discord sticker/emoji); Rose (Discord sticker)</p>
+
+
+
+<p>Zigma Hymn: Meru Thumbs Up (Discord Sticker)</p>
+
+
+
+<p>DrewUniverse: Myconido (Discord sticker / forum avatar)</p>
+
+
+
+<h2 class="wp-block-heading" id="other">Other</h2>
+
+
+
+<p>DrewUniverse: for taming the insanity that it takes to mobilize a legacy fandom.</p>
+</div>
+      </>
+    )
+  }
+  
