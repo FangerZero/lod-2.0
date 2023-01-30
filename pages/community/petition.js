05c1f5da2b8a9ed62a5944f58b9636d1966a554a
Fanger Zero <FangerZero@hotmail.com>
2023-01-30 08:07:39 -0600

    Rough draft of petition

---
+++ pages/community/petition.js
@@ -0,0 +1,107 @@
+import Header from '../../components/layout/header'
+
+export default function Credits() {
+    return (
+      <>
+        <Header title="Petition" description="Petition" />
+        
+        <div class="entry-content wp-block-post-content is-layout-constrained">
+<p><strong>December 2nd, 2019 marks the 20th anniversary for Legend of Dragoon’s original release in Japan.</strong> Now is an excellent time to unite worldwide and petition for a remake. You can sign right on this page! Below the petition is a formal letter addressed to Sony Interactive Entertainment. </p>
+
+
+
+<p><em><strong>Note:</strong></em> <em>if you are not comfortable putting your first name, feel free to write an alias/nickname. </em></p>
+
+
+
+<p><strong>The minimum goal is to attain 11,682 signatures</strong><em> (years since Dragon Campaign)</em>. We have reached our main target of 25,000 signatures! The next target is 50,000 and then 100,000. </p>
+
+Place commented out coce here 
+
+<hr class="wp-block-separator" />
+
+
+
+<p>One fan was so deeply inspired by this campaign, that they recorded a voiceover of the letter. It is done in the style of LoD’s history videos. Voice credit to RAVENSAUDIO.</p>
+
+
+
+<figure class="wp-block-embed aligncenter is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+<iframe loading="lazy" title="Legend of Dragoon Remake Petition - Fan Video" src="https://www.youtube.com/embed/f53uboual2g?feature=oembed" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" class="ui-droppable" width="500" height="281" frameborder="0"></iframe>
+</div></figure>
+
+
+
+<hr class="wp-block-separator" />
+
+
+
+<h3 class="wp-block-heading" id="outreach">Outreach</h3>
+
+
+
+<p>If you use Twitter, <a href="https://twitter.com/Drew_Universe/status/1201389189037789184">please join our Hashtag campaign</a>! Like and RT with the #RemakeLoD hashtag.</p>
+
+
+
+<p>If you have a sphere of influence, small or large, please spread word of our campaign to <em>relevant</em> networks and news outlets. Be sure to do so with respect, however. We do not condone spam or inappropriate advertising. </p>
+
+
+
+<p class="has-text-align-center"><strong>Connect with fellow fans on social media!</strong></p>
+
+
+</div>
+
+      </>
+    )
+}
+
+
+/*
+
+
+<div class="civist-oembed-wrapper-42"><div id="civist-63278a70-385d-440a-9475-b62e76b23c30" style="max-width: 500px;" data-permalink="https://legendofdragoon.org/petition/" data-mode="default" class="civist-widget">	<script type="text/javascript">		var _civist = _civist || [];		_civist.push(['civist-63278a70-385d-440a-9475-b62e76b23c30', 'https://widget.civist.cloud/?api_url=https%3A%2F%2Fapi.civist.cloud%2Ft%2Fb0410bbf-610f-4e8d-a6e4-6655acf80fce%2F#/RW1iZWRkaW5nOjljNjhiZmRlLTA4NzItNDA4MC04OTdjLWFjZWE0ZjZjZmYwOA==']);		if (_civist.length === 1) {			(function() {				var d = document; var s = d.createElement("script");				s.type = "text/javascript";				s.async = true;				s.src = "https://widget.civist.cloud/embed.js";				d.body.appendChild(s);			})();		}	</script>	<noscript style="display: block;">		&lt;span lang="en" style="			text-align: center;			font-family: sans-serif;			color: #222;			background-color: #eee;			border: 1px solid #ddd;			border-radius: 2px;			padding: 20px;			display: block;		"&gt;			&lt;span style="				text-align: center;				font-family: sans-serif;				font-size: 20px;				font-weight: bold;				margin-top: 24px;				margin-bottom: 24px;				padding-bottom: 10px;				border-bottom: 1px solid #aaa;				display: block;			"&gt;JavaScript required&lt;/span&gt;			&lt;span style="				text-align: center;				font-family: sans-serif;				font-size: 14px;				font-weight: regular;				margin-bottom: 10px;				display: block;			"&gt;				We are sorry but without JavaScript enabled, this widget does not work.				Please enable JavaScript in your browser&amp;#8217;s settings and reload this page.			&lt;/span&gt;			&lt;a href="" style="				color: #fff;				background-color: #2196f3;				font-size: 14px;				text-decoration: none !important;				text-transform: uppercase;				padding: 10px 25px;				display: inline-block;				margin-top: 10px;				border: 0;				border-radius: 2px;			"&gt;Reload&lt;/a&gt;		&lt;/span&gt;	</noscript><div class="civist-widget__civist-progress">
+    <div class="civist-progress" style="position: relative">
+      <div class="civist-progress__civist-progress-headings">
+        <div class="civist-progress-headings__civist-progress-headings-name">
+          <div class="civist-progress-headings-name" style="width: 50%; height: auto; float: left;">
+            <h3 class="civist-progress-headings-name__value" style="margin: 0; padding 0;">
+                Signings
+            </h3>
+          </div>
+        </div>
+        <div class="civist-progress-headings__civist-progress-headings-goal">
+          <div class="civist-progress-headings-goal" style="width: 50%; height: auto; float: right;">
+            <h3 class="civist-progress-headings-goal__value" style="position: absolute; right: 0; margin: 0; padding 0;">
+                Goal
+            </h3>
+          </div>
+        </div>
+        <div style="clear: both"></div>
+      </div>
+      <div class="civist-progress__civist-progress-values">
+        <div class="civist-progress-values">
+          <div class="civist-progress-values__civist-progress-values-value" style="width: 50%; height: auto; float: left;">
+            <h4 class="civist-progress-values-value__value" style="margin: 0; padding 0;">29,381</h4>
+          </div>
+          <div class="civist-progress-values__civist-progress-values-goal">
+            <div class="civist-progress-values-goal" style="width: 50%; height: auto; float: right;">
+              <h4 class="civist-progress-values-goal__value" style="position: absolute; right: 0; margin: 0; padding 0;">30,000</h4>
+            </div>
+          </div>
+          <div style="clear: both"></div>
+        </div>
+        <div class="civist-progress__civist-progress-bar" style="margin-bottom: 16px; height: 20px; background-color: #f6f5ee;">
+          <div class="civist-progress-bar" style="width: 97.93666666666667%; height: 100%; background-color: #f2ae48;">
+        </div>
+      </div>
+    </div>
+  </div></div><div class="civist-widget__civist-form" style="width: calc(100% + 32px); margin-left: -16px;">
+    <div class="civist-widget-form" style="position: relative; width: 100%; height: 100%; padding-bottom: 636.5px; transition: padding-bottom 500ms ease-out 0s;">
+      <iframe class="civist-widget-form__iframe ui-droppable" src="https://widget.civist.cloud/?api_url=https%3A%2F%2Fapi.civist.cloud%2Ft%2Fb0410bbf-610f-4e8d-a6e4-6655acf80fce%2F&amp;permalink=https%3A%2F%2Flegendofdragoon.org%2Fpetition%2F&amp;referrer=https%3A%2F%2Flegendofdragoon.org%2Fpetition%2F&amp;mode=default#/RW1iZWRkaW5nOjljNjhiZmRlLTA4NzItNDA4MC04OTdjLWFjZWE0ZjZjZmYwOA==" style="width: 100%; height: 100%; position: absolute; border: 0">
+      </iframe>
+    </div>
+  </div></div></div><script type="text/javascript">  var origin = 'https://widget.civist.cloud/';  origin = origin.slice(0, -1);  var handleEmbedPostMessage = function(embedContainer) {    window.addEventListener('message', function(msg) {      if(msg.origin !== origin || msg.data !== 'ready') {        return;      }      embedContainer.querySelector('iframe').contentWindow.postMessage({        permalink:'https://legendofdragoon.org/petition/',        referrer: window.location.href      },'https://widget.civist.cloud/')    });  };  var handleEmbed = function(embedContainer) {    embedContainer.setAttribute('data-permalink', 'https://legendofdragoon.org/petition/');    embedContainer.setAttribute('data-mode', 'default');  };  var embedsThemeId = document.querySelectorAll('#post-42 [id^=civist]');  var embedsInternalClass = document.querySelectorAll('.civist-oembed-wrapper-42 [id^=civist]');  [].forEach.call(embedsThemeId, handleEmbedPostMessage);  [].forEach.call(embedsInternalClass, handleEmbedPostMessage);  [].forEach.call(embedsThemeId, handleEmbed);  [].forEach.call(embedsInternalClass, handleEmbed);</script>
+
+*/
