08b2450a9b694cafbe482a46ad77e9060182191d
Fanger Zero <FangerZero@hotmail.com>
2023-01-29 22:44:50 -0600

    Adding tests, updated Misc link and name

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -3,40 +3,40 @@ import Link from 'next/link'
 export default function Navigation() {
   return (
     <nav className="sticky top-0 bg-secondary hidden md:flex md:flex-row items-center justify-center z-nav">
-      <Link className="nav-style" href="/">Home</Link>
-      <Link className="nav-style" href="/news">News</Link>
+      <Link className="nav-style" href="/" data-cy="nav-home">Home</Link>
+      <Link className="nav-style" href="/news" data-cy="nav-news">News</Link>
     
       <div className="group">
-        <div className="nav-style">Game</div>
-        <div className="menu-dropdown-content">
-          <Link className="nav-style" href="/game/characters">Characters</Link>
-          <Link className="nav-style" href="/game/map">Map</Link>
-          <Link className="nav-style" href="/game/timeline">Timeline</Link>
-          <Link className="nav-style" href="/game/misc-facts">Miscelaneous Facts</Link>
+        <div className="nav-style" data-cy="nav-game">Game</div>
+        <div className="menu-dropdown-content" data-cy="nav-game-dd">
+          <Link className="nav-style" href="/game/characters" data-cy="nav-characters">Characters</Link>
+          <Link className="nav-style" href="/game/map" data-cy="nav-map">Map</Link>
+          <Link className="nav-style" href="/game/timeline" data-cy="nav-timeline">Timeline</Link>
+          <Link className="nav-style" href="/game/miscelaneous" data-cy="nav-miscelaneous">Miscelaneous</Link>
         </div>
       </div>
 
       <div className="group">
         <div className="nav-style">Community</div>
         <div className="menu-dropdown-content">
-          <Link className="nav-style" href="/community/petition">Petition</Link>
-          <Link className="nav-style" href="/community/projects">Projects</Link>
-          <Link className="nav-style" href="/community/fan-art">Fan Art</Link>
-          <Link className="nav-style" href="/community/forum">Forum</Link>
-          <Link className="nav-style" href="/community/speedruns">Speedruns</Link>
-          <Link className="nav-style" href="/community/credits">Credits</Link>
+          <Link className="nav-style" href="/community/petition" data-cy="nav-petition">Petition</Link>
+          <Link className="nav-style" href="/community/projects" data-cy="nav-projects">Projects</Link>
+          <Link className="nav-style" href="/community/fan-art" data-cy="nav-fanart">Fan Art</Link>
+          <Link className="nav-style" href="/community/forum" data-cy="nav-forum">Forum</Link>
+          <Link className="nav-style" href="/community/speedruns" data-cy="nav-speedruns">Speedruns</Link>
+          <Link className="nav-style" href="/community/credits" data-cy="nav-credits">Credits</Link>
         </div>
       </div>
       
       <div className="group">
         <div className="nav-style">Resources</div>
         <div className="menu-dropdown-content">
-          <Link className="nav-style" href="/resources/downloads">Downloads</Link>
-          <Link className="nav-style" href="/resources/guides">Guides</Link>
-          <Link className="nav-style" href="https://legendofdragoon.org/wiki/index.php/Main_Page">Wiki</Link>
-          <Link className="nav-style" href="/resources/damage-calculator">Damage Calculator</Link>
-          <Link className="nav-style" href="/resources/contact">Contact</Link>
-          <Link className="nav-style" href="/resources/archives">Archives</Link>
+          <Link className="nav-style" href="/resources/downloads" data-cy="nav-downloads">Downloads</Link>
+          <Link className="nav-style" href="/resources/guides" data-cy="nav-guides">Guides</Link>
+          <Link className="nav-style" href="https://legendofdragoon.org/wiki/index.php/Main_Page" data-cy="nav-wiki">Wiki</Link>
+          <Link className="nav-style" href="/resources/damage-calculator" data-cy="nav-dmgCalc">Damage Calculator</Link>
+          <Link className="nav-style" href="/resources/contact" data-cy="nav-contact">Contact</Link>
+          <Link className="nav-style" href="/resources/archives" data-cy="nav-archives">Archives</Link>
         </div>
       </div>
     </nav>
--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,7 +1,10 @@
 
 describe('Landing Page', () => {
-    it('Theme Component: Changing themes', () => {
+    /*before(() => {
         cy.visit('localhost:3000/')
+    })*/
+    it('Theme Component: Changing themes', () => {
+         cy.visit('localhost:3000/')
         // Check Body BG Color
         cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)')
         cy.get('[data-cy="theme"]').should('exist')
@@ -30,5 +33,22 @@ describe('Landing Page', () => {
         // Make sure BG and "current" img changed to "darkness"
         cy.get('body').should('have.css', 'background-color', 'rgb(42, 40, 90)')
         cy.get('[data-cy="current"]').should('have.attr', 'src', '/images/bullets/DS_Darkness.webp')
+    })
+
+    it('Utilizing the Navigational Bar', () => {
+        cy.visit('localhost:3000/')
+        cy.get('[data-cy="current"]').should('have.attr', 'src', '/images/bullets/default.webp')
+        // Go to Home Page
+        cy.get('a[data-cy="nav-home"]').should('be.visible').should('have.length', 1).click()
+        cy.url().should('eq', 'http://localhost:3000/') 
+        // Go to News Page
+        cy.get('a[data-cy="nav-news"]').should('be.visible').should('have.length', 1).click()
+        cy.url().should('eq', 'http://localhost:3000/news') 
+        // Go to Map Page Will need to manually invoke show, Cypress does not do "hover"
+        
+        cy.get('[data-cy="nav-game-dd"]').invoke('show')
+        cy.get('a[data-cy="nav-map"]').should('be.visible').should('have.length', 1).click()
+        cy.url().should('eq', 'http://localhost:3000/game/map') 
+        
     })
   })
