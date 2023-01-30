a85a3ffd48b584a28c56c88acb821adf38cba70a
Fanger Zero <FangerZero@hotmail.com>
2023-01-29 23:09:54 -0600

    Added Timeline file

---
+++ pages/game/timeline.js
@@ -0,0 +1,16 @@
+import Header from '../../components/layout/header'
+import Merch from '../../components/game/merch'
+import Misc from '../../components/game/misc'
+import Promos from '../../components/game/promos'
+import { useState } from 'react'
+
+export default function Miscelaneous() {
+  return (
+    <>
+      <Header title="Timeline" description="Timeline information" />
+      This will be a verticle timeline to help w/ the long history that Legend of Dragoon encapsulates.
+      <br />
+      This is going to take a while to determine and will need test data. 
+    </>
+  )
+}
