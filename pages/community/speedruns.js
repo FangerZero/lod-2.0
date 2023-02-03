e6dbed40f4647abadb9edf2279188f285740675a
Fanger Zero <FangerZero@hotmail.com>
2023-02-02 22:30:40 -0600

    Added Speed Runners page with some info

---
+++ pages/community/speedruns.js
@@ -0,0 +1,26 @@
+import Header from '../../components/layout/header'
+
+export default function Speedruns() {
+    return (
+      <>
+      <Header title="Speed Runs" description="Giving the community that helps make this game thrive years after others have forgotten it." />
+      <div class="entry-content wp-block-post-content is-layout-constrained">
+            <h3>A brief history from release to 2022</h3>
+            <p>Intro paragraph—</p>
+            <p>The following video portrays a brief history of speedrunning in Legend of Dragoon, leading up to the latest breakthrough in run length.</p>
+            <figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+            <iframe loading="lazy" title="How Speedrunners Got To Legend Of Dragoon's Debug Menu Without Cheat Codes" src="https://www.youtube.com/embed/15gtlChBYE8?feature=oembed" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" class="ui-droppable" width="500" height="281" frameborder="0"></iframe>
+            </div></figure>
+            <hr />
+            <h3>2022</h3>
+            <p>For over a decade, a potential way to drastically shorten the speedrun route was stuck at a dead-end. LoD’s debug room could be accessed through the save-over glitch, but it left the party at level 0 and the first battle would result in a game over since Dart had no hit points (0/0). However, in early 2022, progress was made at long last. This gave players the new option of a skip-included run. Already, a time of less than four hours has been recorded!</p>
+            <hr />
+            <h3>Summary Info</h3>
+            <p><a href="https://www.speedrun.com/the_legend_of_dragoon" target="_blank" rel="noreferrer noopener">LoD Speedrun leaderboards</a></p>
+            <p>Current World Records</p>
+            <a rel="noreferrer noopener" href="https://www.speedrun.com/the_legend_of_dragoon#Any" target="_blank">Any%</a>: 7h39m22s by <a rel="noreferrer noopener" href="https://www.speedrun.com/user/DillonWithAnI" target="_blank">DillonWithAnI</a><br />Any% Turbo: 3h46m04s by <a rel="noreferrer noopener" href="https://www.speedrun.com/user/blackdeathdoom" target="_blank">blackdeathdoom</a><br /><a rel="noreferrer noopener" href="https://www.speedrun.com/the_legend_of_dragoon#Any_Classic" target="_blank">Any% Classic</a>: 10h31m49s by <a rel="noreferrer noopener" href="https://www.speedrun.com/user/blackdeathdoom" target="_blank">blackdeathdoom</a><br />Any% Classic Turbo: 10h13m03s by <a rel="noreferrer noopener" href="https://www.speedrun.com/user/lasagnaaammm" target="_blank">lasagnaaammm</a><br /><a rel="noreferrer noopener" href="https://www.speedrun.com/the_legend_of_dragoon#Any_JP" target="_blank">Any% JP Classic</a>: 13h47m32s by <a href="https://www.speedrun.com/user/blackdeathdoom" target="_blank" rel="noreferrer noopener">blackdeathdoom</a> <br /><a href="https://www.speedrun.com/the_legend_of_dragoon#Magician_Faust" target="_blank" rel="noreferrer noopener">Magician Faust</a>: 9h13m26s by <a href="https://www.speedrun.com/user/blackdeathdoom" target="_blank" rel="noreferrer noopener">blackdeathdoom</a>
+        </div>
+      </>
+    )
+  }
+  
