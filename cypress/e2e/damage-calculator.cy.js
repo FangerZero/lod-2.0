d617280dc720b0c5a2660968f7b905330781db8c
Fanger Zero <FangerZero@hotmail.com>
2023-02-02 20:55:34 -0600

    Updated Tests

--- cypress/e2e/damage-calculator.cy.js
+++ cypress/e2e/damage-calculator.cy.js
@@ -4,107 +4,107 @@ describe('Damage Calculator', () => {
   })
 
   it('On Screen And Validation', () => {
-    cy.get('[data-cy="player"]').should('exist')
-    cy.get('[data-cy="playerLevel"]').should('exist')
-    cy.get('[data-cy="attackStat"]').should('exist')
-    cy.get('[data-cy="addition"]').should('exist')
-    cy.get('[data-cy="maxHitPercent"]').should('not.exist')
-    cy.get('[data-cy="dragoonModifier"]').should('not.exist')
-    cy.get('[data-cy="enemy"]').should('exist')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: ')
+    cy.getBySel('player').should('exist')
+    cy.getBySel('playerLevel').should('exist')
+    cy.getBySel('attackStat').should('exist')
+    cy.getBySel('addition').should('exist')
+    cy.getBySel('maxHitPercent').should('not.exist')
+    cy.getBySel('dragoonModifier').should('not.exist')
+    cy.getBySel('enemy').should('exist')
+    cy.getBySel('damage').should('exist').contains('Damage: ')
     
     //invalid Level Data
-    cy.get('[data-cy="playerLevel"]').clear().type(100)
-    cy.get('[data-cy="playerLevel"]').should('have.css', 'border-width', '8px')
-    cy.get('[data-cy="playerLevel"]').clear().type(99)
-    cy.get('[data-cy="playerLevel"]').should('have.css', 'border-width', '1px')
+    cy.getBySel('playerLevel').clear().type(100)
+    cy.getBySel('playerLevel').should('have.css', 'border-width', '8px')
+    cy.getBySel('playerLevel').clear().type(99)
+    cy.getBySel('playerLevel').should('have.css', 'border-width', '1px')
     //invalid Attack Data
-    cy.get('[data-cy="attackStat"]').clear().type(1000)
-    cy.get('[data-cy="attackStat"]').should('have.css', 'border-width', '8px')
-    cy.get('[data-cy="attackStat"]').clear().type(75)
-    cy.get('[data-cy="attackStat"]').should('have.css', 'border-width', '1px')
+    cy.getBySel('attackStat').clear().type(1000)
+    cy.getBySel('attackStat').should('have.css', 'border-width', '8px')
+    cy.getBySel('attackStat').clear().type(75)
+    cy.getBySel('attackStat').should('have.css', 'border-width', '1px')
   })
 
   it('Rose - Astral Drain - Cactus', () => {
-    cy.get('[data-cy="player"]').select('Rose')
-    cy.get('[data-cy="playerLevel"]').type(33)
-    cy.get('[data-cy="attackStat"]').type(113)
-    cy.get('[data-cy="addition"]').select('Astral Drain')
-    cy.get('[data-cy="maxHitPercent"]').should('not.exist')
-    cy.get('[data-cy="dragoonModifier"]').should('exist')
-    cy.get('[data-cy="dragoonModifier"]').select(5)
-    cy.get('[data-cy="enemy"]').select('Cactus')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 486') // 486
-    cy.get('[data-cy="field"]').select('Fire')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 486') // 486
-    cy.get('[data-cy="field"]').select('Dark')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 729') // 728
+    cy.getBySel('player').select('Rose')
+    cy.getBySel('playerLevel').type(33)
+    cy.getBySel('attackStat').type(113)
+    cy.getBySel('addition').select('Astral Drain')
+    cy.getBySel('maxHitPercent').should('not.exist')
+    cy.getBySel('dragoonModifier').should('exist')
+    cy.getBySel('dragoonModifier').select(5)
+    cy.getBySel('enemy').select('Cactus')
+    cy.getBySel('damage').should('exist').contains('Damage: 486') // 486
+    cy.getBySel('field').select('Fire')
+    cy.getBySel('damage').should('exist').contains('Damage: 486') // 486
+    cy.getBySel('field').select('Dark')
+    cy.getBySel('damage').should('exist').contains('Damage: 729') // 728
   })
   
   it('Rose - Whip Smack', () => {
-    cy.get('[data-cy="player"]').select('Rose')
-    cy.get('[data-cy="playerLevel"]').type(12)
-    cy.get('[data-cy="attackStat"]').type(67)
-    cy.get('[data-cy="addition"]').select('Whip Smack')
-    cy.get('[data-cy="dragoonModifier"]').should('not.exist')
-    cy.get('[data-cy="maxHitPercent"]').should('exist')
-    cy.get('[data-cy="maxHitPercent"]').select(2)
-    cy.get('[data-cy="enemy"]').select('Hellena Warden')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 71') // 71
-    cy.get('[data-cy="enemy"]').select('Senior Warden')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 54') // 54
+    cy.getBySel('player').select('Rose')
+    cy.getBySel('playerLevel').type(12)
+    cy.getBySel('attackStat').type(67)
+    cy.getBySel('addition').select('Whip Smack')
+    cy.getBySel('dragoonModifier').should('not.exist')
+    cy.getBySel('maxHitPercent').should('exist')
+    cy.getBySel('maxHitPercent').select(2)
+    cy.getBySel('enemy').select('Hellena Warden')
+    cy.getBySel('damage').should('exist').contains('Damage: 71') // 71
+    cy.getBySel('enemy').select('Senior Warden')
+    cy.getBySel('damage').should('exist').contains('Damage: 54') // 54
   })
 
   it('Lavitz - Rod Typhoon', () => {
-    cy.get('[data-cy="player"]').select('Lavitz')
-    cy.get('[data-cy="playerLevel"]').type(12)
-    cy.get('[data-cy="attackStat"]').type(60)
-    cy.get('[data-cy="addition"]').select('Rod Typhoon')
-    cy.get('[data-cy="dragoonModifier"]').should('not.exist')
-    cy.get('[data-cy="maxHitPercent"]').should('exist')
-    cy.get('[data-cy="maxHitPercent"]').select(4)
-    cy.get('[data-cy="enemy"]').select('Plague Rat')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 157') // 157
-    cy.get('[data-cy="enemy"]').select('Hellena Warden')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 94') // 94
-    cy.get('[data-cy="enemy"]').select('Senior Warden')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 73') // 73
+    cy.getBySel('player').select('Lavitz')
+    cy.getBySel('playerLevel').type(12)
+    cy.getBySel('attackStat').type(60)
+    cy.getBySel('addition').select('Rod Typhoon')
+    cy.getBySel('dragoonModifier').should('not.exist')
+    cy.getBySel('maxHitPercent').should('exist')
+    cy.getBySel('maxHitPercent').select(4)
+    cy.getBySel('enemy').select('Plague Rat')
+    cy.getBySel('damage').should('exist').contains('Damage: 157') // 157
+    cy.getBySel('enemy').select('Hellena Warden')
+    cy.getBySel('damage').should('exist').contains('Damage: 94') // 94
+    cy.getBySel('enemy').select('Senior Warden')
+    cy.getBySel('damage').should('exist').contains('Damage: 73') // 73
   })
 
   it('Meru - Freezing Ring - Cactus', () => {
-    cy.get('[data-cy="player"]').select('Meru')
-    cy.get('[data-cy="playerLevel"]').type(32)
-    cy.get('[data-cy="attackStat"]').type(143)
-    cy.get('[data-cy="addition"]').select('Freezing Ring')
-    cy.get('[data-cy="maxHitPercent"]').should('not.exist')
-    cy.get('[data-cy="dragoonModifier"]').should('exist')
-    cy.get('[data-cy="dragoonModifier"]').select(5)
-    cy.get('[data-cy="enemy"]').select('Cactus')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 599') // 598
-    cy.get('[data-cy="field"]').select('Fire')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 299') // 298
-    cy.get('[data-cy="field"]').select('Dark')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 599') // 598
-    cy.get('[data-cy="field"]').select('Water')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 898')
+    cy.getBySel('player').select('Meru')
+    cy.getBySel('playerLevel').type(32)
+    cy.getBySel('attackStat').type(143)
+    cy.getBySel('addition').select('Freezing Ring')
+    cy.getBySel('maxHitPercent').should('not.exist')
+    cy.getBySel('dragoonModifier').should('exist')
+    cy.getBySel('dragoonModifier').select(5)
+    cy.getBySel('enemy').select('Cactus')
+    cy.getBySel('damage').should('exist').contains('Damage: 599') // 598
+    cy.getBySel('field').select('Fire')
+    cy.getBySel('damage').should('exist').contains('Damage: 299') // 298
+    cy.getBySel('field').select('Dark')
+    cy.getBySel('damage').should('exist').contains('Damage: 599') // 598
+    cy.getBySel('field').select('Water')
+    cy.getBySel('damage').should('exist').contains('Damage: 898')
   })
 
   it('Dart Feld - Flameshot - Cactus', () => {
-    cy.get('[data-cy="player"]').select('Dart Feld')
-    cy.get('[data-cy="playerLevel"]').type(32)
-    cy.get('[data-cy="attackStat"]').type(113)
-    cy.get('[data-cy="addition"]').select('Flameshot')
-    cy.get('[data-cy="maxHitPercent"]').should('not.exist')
-    cy.get('[data-cy="dragoonModifier"]').should('exist')
-    cy.get('[data-cy="dragoonModifier"]').select(5)
-    cy.get('[data-cy="enemy"]').select('Cactus')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 472') // 472
-    cy.get('[data-cy="field"]').select('Fire')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 708') // 708
-    cy.get('[data-cy="field"]').select('Dark')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 472') // 472
-    cy.get('[data-cy="field"]').select('Water')
-    cy.get('[data-cy="damage"]').should('exist').contains('Damage: 236')
+    cy.getBySel('player').select('Dart Feld')
+    cy.getBySel('playerLevel').type(32)
+    cy.getBySel('attackStat').type(113)
+    cy.getBySel('addition').select('Flameshot')
+    cy.getBySel('maxHitPercent').should('not.exist')
+    cy.getBySel('dragoonModifier').should('exist')
+    cy.getBySel('dragoonModifier').select(5)
+    cy.getBySel('enemy').select('Cactus')
+    cy.getBySel('damage').should('exist').contains('Damage: 474') // 472
+    cy.getBySel('field').select('Fire')
+    cy.getBySel('damage').should('exist').contains('Damage: 711') // 708
+    cy.getBySel('field').select('Dark')
+    cy.getBySel('damage').should('exist').contains('Damage: 472') // 472
+    cy.getBySel('field').select('Water')
+    cy.getBySel('damage').should('exist').contains('Damage: 236')
   })
   
 })
--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -7,47 +7,47 @@ describe('Landing Page', () => {
          cy.visit('localhost:3000/')
         // Check Body BG Color
         cy.get('body').should('have.css', 'background-color', 'rgb(255, 255, 255)')
-        cy.get('[data-cy="theme"]').should('exist')
+        cy.getBySel('theme').should('exist')
         // Spirits should be hidden until clicked
-        cy.get('[data-cy="spirits"]').should('not.be.visible')
-        cy.get('[data-cy="current"]').click()
-        cy.get('[data-cy="spirits"]').should('be.visible')
+        cy.getBySel('spirits').should('not.be.visible')
+        cy.getBySel('current').click()
+        cy.getBySel('spirits').should('be.visible')
 
         //          Red-Eye Theme
         // Make sure Red Exists before clicking, then Hide Spirits
-        cy.get('[data-cy="red"]').should('exist')
-        cy.get('[data-cy="red"]').click()
-        cy.get('[data-cy="spirits"]').should('not.be.visible')
+        cy.getBySel('red').should('exist')
+        cy.getBySel('red').click()
+        cy.getBySel('spirits').should('not.be.visible')
         // Make sure BG and "current" img changed to "red"
         cy.get('body').should('have.css', 'background-color', 'rgb(255, 54, 0)')
-        cy.get('[data-cy="current"]').should('have.attr', 'src', '/images/bullets/DS_Red-Eye.webp')
+        cy.getBySel('current').should('have.attr', 'src', '/images/bullets/DS_Red-Eye.webp')
 
         //          Darkness Theme
         // Spirits should be hidden until clicked
-        cy.get('[data-cy="current"]').click()
-        cy.get('[data-cy="spirits"]').should('be.visible')
+        cy.getBySel('current').click()
+        cy.getBySel('spirits').should('be.visible')
         // Make sure darkness Exists before clicking, then Hide Spirits
-        cy.get('[data-cy="darkness"]').should('exist')
-        cy.get('[data-cy="darkness"]').click()
-        cy.get('[data-cy="spirits"]').should('not.be.visible')
+        cy.getBySel('darkness').should('exist')
+        cy.getBySel('darkness').click()
+        cy.getBySel('spirits').should('not.be.visible')
         // Make sure BG and "current" img changed to "darkness"
         cy.get('body').should('have.css', 'background-color', 'rgb(42, 40, 90)')
-        cy.get('[data-cy="current"]').should('have.attr', 'src', '/images/bullets/DS_Darkness.webp')
+        cy.getBySel('current').should('have.attr', 'src', '/images/bullets/DS_Darkness.webp')
     })
 
     it('Utilizing the Navigational Bar', () => {
         cy.visit('localhost:3000/')
-        cy.get('[data-cy="current"]').should('have.attr', 'src', '/images/bullets/default.webp')
+        cy.getBySel('current').should('have.attr', 'src', '/images/bullets/default.webp')
         // Go to Home Page
-        cy.get('a[data-cy="nav-home"]').should('be.visible').should('have.length', 1).click()
+        cy.getBySel('nav-home').should('be.visible').should('have.length', 1).click()
         cy.url().should('eq', 'http://localhost:3000/') 
         // Go to News Page
-        cy.get('a[data-cy="nav-news"]').should('be.visible').should('have.length', 1).click()
+        cy.getBySel('nav-news').should('be.visible').should('have.length', 1).click()
         cy.url().should('eq', 'http://localhost:3000/news') 
         // Go to Map Page Will need to manually invoke show, Cypress does not do "hover"
         
-        cy.get('[data-cy="nav-game-dd"]').invoke('show')
-        cy.get('a[data-cy="nav-map"]').should('be.visible').should('have.length', 1).click()
+        cy.getBySel('nav-game-dd').invoke('show')
+        cy.getBySel('nav-map').should('be.visible').should('have.length', 1).click()
         cy.url().should('eq', 'http://localhost:3000/game/map') 
         
     })
